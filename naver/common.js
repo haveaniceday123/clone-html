f430789d8f7ba9754f21658b1c48fef6113e15f3
parks <higod4000@naver.com>
2020-08-13 13:50:52 +0900

    머지전 커밋

--- naver/common.js
+++ naver/common.js
@@ -1,12 +1,3 @@
-var tests = 1;
-
-// function test() {
-//   event.preventDefault();
-//   event.stopPropagation();
-//   $(".pop-up-wrapper").css('display', 'none');
-// }
-
-
 $(document).ready(function() {
   getCookieMobile();
   $(".pop-up-close-button").click(function() {
@@ -20,21 +11,16 @@ $(document).ready(function() {
 
 function setCookieMobile( name, value, expiredays) {
   var todayDate = new Date();
-  console.log(escape(value), '<==============');
   todayDate.setDate( todayDate.getDate() + expiredays );
   document.cookie = name + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";"
-  console.log(document.cookie, '<==============');
 }
 
 function getCookieMobile() {
-  console.log(2);
   var cookieData = document.cookie;
   if (cookieData.indexOf("todayCookie=done") < 0) {
-    console.log('cookie not exists');
     $(".pop-up-wrapper").show();
   } 
   else {
     $(".pop-up-wrapper").hide();
-    console.log('cookie exists');
   }
 }

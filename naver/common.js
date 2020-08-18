9709fc6c1dff253f9a55af315a5256311ab272d5
parks <higod4000@naver.com>
2020-08-18 09:36:54 +0900

    Save my work

--- naver/common.js
+++ naver/common.js
@@ -1,67 +1,157 @@
-// All code and comments were written by Jinyong Park.
-// it turns out that this is for learning purposes only and not commercial
+/**
+ * 
+ * it turns out that this is for learning purpose only and not commercial.
+ * @author Jinyong park.
+ */
 
-$(document).ready(function() {
-  getCookieMobile();
-  $(".pop-up-close-button").click(function() {
-    event.preventDefault();
-    event.stopPropagation();
-    setCookieMobile("todayCookie", "done", 1);
-    $(".pop-up-wrapper").hide();
-  })
-});
+ var popUpCookieName = "pop-up-banner";
 
-function setCookieMobile( name, value, expiredays) {
-  var todayDate = new Date();
-  todayDate.setDate( todayDate.getDate() + expiredays );
-  document.cookie = name + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";"
-}
-
-function getCookieMobile() {
-  var cookieData = document.cookie;
-  if (cookieData.indexOf("todayCookie=done") < 0) {
-    $(".pop-up-wrapper").show();
+/**
+ * when all the documents are ready, decide whether to show the 
+ * banner or not.
+ */
+$(window).ready(function () {
+  var autoCompelete = $('.pop-up-wrapper');
+  if (getCookie(popUpCookieName)) {
+    autoCompelete.hide();
   } 
   else {
-    $(".pop-up-wrapper").hide();
+    autoCompelete.show();
   }
+});
+
+$(window).onload(function() {
+  alert("hi");
+})
+
+/**
+ * when button click, "not showing 3 days" event
+ * event prevents and close pop-up-banner.
+ */
+$('.pop-up-close-button').click(function(event) {
+  eventPrevent(event);
+  setCookie(popUpCookieName, "ok", 1);
+  $('.pop-up-wrapper').hide();
+})
+
+/**
+ * user key press enter, the input saving .txt file;
+ */
+$('#main-input').keydown(function(event) {
+  if (event.keyCode === 13) {
+    console.log(event);
+  }
+})
+
+/**
+ * 
+ * @param {*} name cookie name for searching
+ * @return {object} object contain key, value. if not contain, it will return false
+ */
+function getCookie(name) {
+  var currentSavingCookie = isHasCookieData(name);
+  if (currentSavingCookie === undefined) return false;
+  var splitResult = currentSavingCookie.split("=");
+
+  return {
+    key: splitResult[0],
+    value: splitResult[1],
+  };
 }
 
-// auto compelete on / off function
+/**
+ * 
+ * @param {*} name cookie's key
+ * @returns {string} it will return cookie that has "name"
+ */
+function isHasCookieData(name) {
+  var cookieData = document.cookie.replace(/;/g, "").split(" ");
+  var cookieSplitKeyValue = cookieData.find((cookie) => {
+    var splitKeyValue = cookie.split("=");
+    return splitKeyValue[0] === name;
+  })
+  return cookieSplitKeyValue;
+}
+
+/**
+ * Save cookie data
+ * @param {*} name cookie data name;
+ * @param {*} value cookie data value;
+ * @param {*} expire cookie data's Expiration date (per minutes)
+ */
+function setCookie(name, value, expire) {
+  var expireDate = new Date();
+  expireDate.setMinutes(expireDate.getMinutes() + expire);
+  document.cookie =
+    escape(name) +
+    "=" +
+    escape(value) +
+    "; path=/; expires=" +
+    expireDate.toGMTString() +
+    ";";
+}
+
+/**
+ * Prevent event propagtion
+ */
+function eventPrevent(event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var autoCompelete = false;
 
 //is this @param's area?
-$('html').click(function(e) {
-  if (!$(e.target).closest(".search-form")[0]) showOffAutoComplete()
+$("html").click(function (e) {
+  if (!$(e.target).closest(".search-form")[0]) showOffAutoComplete();
 });
 
 //when #main-input key-down event
 //if #main-input value is not empty, show auto Complete
 //if not, not show auto Complete
-$('#main-input').keyup(function(e) {
+$("#main-input").keyup(function (e) {
   if ($("#main-input").val() !== "") showAutoComplete();
   else showOffAutoComplete();
-})
+});
 
 //show auto-complete
 function showAutoComplete() {
   autoCompelete = true;
-  $(".down-arrow").addClass('reverse');
-  $(".auto-input-wrapper").css('display', 'block');
+  $(".down-arrow").addClass("reverse");
+  $(".auto-input-wrapper").css("display", "block");
 }
 
 function showOffAutoComplete() {
   autoCompelete = false;
-  $(".down-arrow").removeClass('reverse');
-  $(".auto-input-wrapper").css('display', 'none');
+  $(".down-arrow").removeClass("reverse");
+  $(".auto-input-wrapper").css("display", "none");
 }
 
-$(".down-arrow").click(function(){
+$(".down-arrow").click(function () {
   showAutoComplete();
 });
 
-$(".reverse").click(function(){
+$(".reverse").click(function () {
   showOffAutoComplete();
-})
-
-
+});
